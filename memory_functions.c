061ac219ff5d38f3f4588f210b337526ec1a0c87
Farahhammam22 <hammamfarah6262gmail.com>
2023-10-18 13:35:49 -0700

    a

---
+++ memory_functions.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+		s[j] = b;
+	return (s);
+}
+
+
+void ffree(char **xx)
+{
+	char **a = xx;
+
+	if (!xx)
+		return;
+	while (*xx)
+		free(*xx++);
+	free(a);
+}
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
---
+++ memory_functions2.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+
+int bfree(void **p)
+{
+	if (p && *p)
+	{
+		free(*p);
+		*p = NULL;
+		return (1);
+	}
+	return (0);
+}
